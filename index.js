f15aad5ad3727fc530a0efc9ff5d7ac190980b59
Your Name <you@example.com>
2019-04-03 16:30:00 -0400

    added index.html, index.js, and main.css

---
+++ index.js
@@ -0,0 +1,55 @@
+const QUIZ = [
+    {
+        question: 'John Carmack and John Romero developed which of these games?',
+        optionA: 'myst',
+        optionB: 'ultima',
+        optionC: 'doom',
+        optionD: 'diablo',
+    },
+    {
+        question: 'Who developed Pac-Man?',
+        optionA: 'Toru Iwatani',
+        optionB: 'Shigeru Miyamoto',
+        optionC: 'Shinji Mikami',
+        optionD: 'Keiji Inafune'
+    }, 
+    {
+        question: 'Who developed Pac-Man?',
+        optionA: 'Toru Iwatani',
+        optionB: 'Shigeru Miyamoto',
+        optionC: 'Shinji Mikami',
+        optionD: 'Keiji Inafune'
+    }, 
+    {
+        question: 'Who developed Pac-Man?',
+        optionA: 'Toru Iwatani',
+        optionB: 'Shigeru Miyamoto',
+        optionC: 'Shinji Mikami',
+        optionD: 'Keiji Inafune'
+    },
+    {
+        question: 'Who developed Pac-Man?',
+        optionA: 'Toru Iwatani',
+        optionB: 'Shigeru Miyamoto',
+        optionC: 'Shinji Mikami',
+        optionD: 'Keiji Inafune'
+    }, 
+    {
+        question: 'Who developed Pac-Man?',
+        optionA: 'Toru Iwatani',
+        optionB: 'Shigeru Miyamoto',
+        optionC: 'Shinji Mikami',
+        optionD: 'Keiji Inafune'
+    },
+    {
+        question: 'Who developed Pac-Man?',
+        optionA: 'Toru Iwatani',
+        optionB: 'Shigeru Miyamoto',
+        optionC: 'Shinji Mikami',
+        optionD: 'Keiji Inafune'
+    } 
+];
+
+function renderCurrentQuestion(){
+
+}
